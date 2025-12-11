dab65a7aff67adbe87d6b22bfbf3855883d6f781
ngnayoufabiola <ngnayoufabiola@gmail.com>
2025-12-11 07:40:56 -0800

    add file

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const mobileMenu =document.getElementById ("mobile_menu");
+mobileMenu.addEventListener("click",()=>{ 
+ const desktopMenu = document.getElementById("desktop_menu");
+mobileMenu.classList.to("active");
+ desktopMenu.classList.toggle("active");
+ }
+     )
+
+
+     
+
