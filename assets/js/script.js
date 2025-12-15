9b3d27ffc301e4f9409b3a29cb705cc7318b0ebe
njobe love <njobelovelinenkeni@gmail.com>
2025-12-15 05:50:59 -0800

    added the project-card file in the script.js file, to implement its intaractions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
-const mobileMenu =document.getElementById ("mobile_menu");
-mobileMenu.addEventListener("click",()=>{ 
+//implementation of mobile menu
+const mobileMenu = document.getElementById ("mobile_menu");
+mobileMenu.addEventListener("click",() => { 
  const desktopMenu = document.getElementById("desktop_menu");
 mobileMenu.classList.to("active");
  desktopMenu.classList.toggle("active");
- }
-     )
+ });
+   // an array of object to store project cards
+const projects = [
+    {
+        Image: '#',
+        title: 'Multi-Post Stories Gain+Glory',
+        technologies: ['Ruby on Rails', 'css', 'JavaScript', 'HTML'],
+        button: '#'
+
+    },
+    
+];
 
 
      
