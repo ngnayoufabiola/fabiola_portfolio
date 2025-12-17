5d6649fadce0c0af22930cba3fb5bc7ac9323d4f
ngnayoufabiola <ngnayoufabiola@gmail.com>
2025-12-17 02:14:27 -0800

    stash files

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,6 @@ const mobileMenu =document.getElementById ("mobile_menu");
 mobileMenu.addEventListener("click",()=>{ 
  const desktopMenu = document.getElementById("desktop_menu");
 mobileMenu.classList.to("active");
- desktopMenu.classList.toggle("active");
+  desktopMenu.classList.toggle("active");
  }
      )
-
-
-     
-
